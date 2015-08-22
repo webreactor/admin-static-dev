e0911142031586322cd10c19c78595a2514d4355
Ivan Podorozhny <ivan@podorozhny.ru>
2015-08-23 01:18:19 +0300

    removed responsive part 2; gulpfile refactoring; gulp-data usage refactoring; sidebar childs

--- gulpfile.js
+++ gulpfile.js
@@ -11,93 +11,105 @@ var config      = require('./gulp-config.json'),
     gulpData    = require('gulp-data'),
     gulpCsso    = require('gulp-csso'),
     gulpLess    = require('gulp-less'),
-    gulpJade    = require('gulp-jade');
+    gulpJade    = require('gulp-jade'),
+    browserSync = require('browser-sync').create();
+
+var handleError = function (error) {
+    console.log(error.toString());
+
+    this.emit('end');
+};
+
+var getJsonData = function (file, callback) {
+    var jsonFiles = [];
+
+    if (typeof config.data.core !== 'undefined') {
+        jsonFiles = jsonFiles.concat(config.data.core);
+    }
+
+    var filename = path.basename(file.path, '.jade');
+
+    if (typeof config.data[filename] !== 'undefined') {
+        jsonFiles = jsonFiles.concat(config.data[filename]);
+    }
+
+    jsonFiles = jsonFiles.filter(function (item, pos) {
+        return jsonFiles.indexOf(item) == pos;
+    });
+
+    var jsonData = {};
+
+    for (var i = 0, l = jsonFiles.length; i < l; i++) {
+        var exist = fs.existsSync(jsonFiles[i]);
+
+        if (!exist) {
+            continue;
+        }
+
+        var json = JSON.parse(fs.readFileSync(jsonFiles[i], { encoding: 'utf8' }));
+
+        for (var attr in json) {
+            if (json.hasOwnProperty(attr)) {
+                jsonData[attr] = json[attr];
+            }
+        }
+    }
+
+    return callback(null, jsonData);
+};
 
 gulp.task('css-app', function () {
     return gulp.src(config.build.src.css.app)
         .pipe(gulpLess())
+        .on('error', handleError)
         .pipe(gulpCsso())
-        .pipe(gulpWrapper({header: '\n/* ${filename} */\n\n'}))
+        .pipe(gulpWrapper({ header: '\n/* ${filename} */\n\n' }))
         .pipe(gulpConcat('app.css'))
-        .pipe(gulp.dest(config.build.dest.css));
+        .pipe(gulp.dest(config.build.dest.css))
+        .pipe(browserSync.stream());
 });
 
 gulp.task('css', ['css-app']);
 
-gulp.task('html', function () {
-    return gulp.src(config.build.src.html)
-        .pipe(gulpData(function (file, cb) {
-            var filename = path.basename(file.path, '.jade');
-
-            if (filename == 'index' || filename == 'product') {
-                filename = 'categories';
-            }
-
-            var jsonPath = './assets/html/data/' + filename + '.json';
-
-            fs.exists(jsonPath, function (exists) {
-                if (exists) {
-                    fs.readFile(jsonPath, {encoding: 'utf8'}, function (err, data) {
-                        var json;
-                        if (err) return cb(err);
-                        try {
-                            json = JSON.parse(data);
-                        } catch (e) {
-                            return cb(e);
-                        }
-                        cb(null, json);
-                    });
-                } else
-                    cb(null, {});
-            });
-        }))
+gulp.task('templates', function () {
+    return gulp.src(config.build.src.templates)
+        .pipe(gulpData(getJsonData))
         .pipe(gulpJade({
             jade:   jade,
             pretty: true
         }))
-        .pipe(gulp.dest(config.build.dest.html));
+        .on('error', handleError)
+        .pipe(gulp.dest(config.build.dest.templates))
+        .pipe(browserSync.stream());
 });
 
 gulp.task('js-app', function () {
     return gulp.src(config.build.src.js.app)
         .pipe(gulpUglify())
-        .pipe(gulpWrapper({header: '\n// ${filename}\n\n'}))
+        .pipe(gulpWrapper({ header: '\n// ${filename}\n\n' }))
         .pipe(gulpConcat('app.js'))
-        .pipe(gulp.dest(config.build.dest.js));
+        .pipe(gulp.dest(config.build.dest.js))
+        .pipe(browserSync.stream());
 });
 
-gulp.task('js-catalog', function () {
-    return gulp.src(config.build.src.js.catalog)
-        .pipe(gulpUglify())
-        .pipe(gulpWrapper({header: '\n// ${filename}\n\n'}))
-        .pipe(gulpConcat('catalog.js'))
-        .pipe(gulp.dest(config.build.dest.js));
-});
-
-gulp.task('js-product-view', function () {
-    return gulp.src(config.build.src.js.product_view)
-        .pipe(gulpUglify())
-        .pipe(gulpWrapper({header: '\n// ${filename}\n\n'}))
-        .pipe(gulpConcat('product-view.js'))
-        .pipe(gulp.dest(config.build.dest.js));
-});
-
-gulp.task('js', ['js-app', 'js-catalog', 'js-product-view']);
+gulp.task('js', ['js-app']);
 
 gulp.task('img', function () {
     return gulp.src(config.build.src.img)
         .pipe(gulpNewer(config.build.dest.img))
-        .pipe(gulp.dest(config.build.dest.img));
+        .pipe(gulp.dest(config.build.dest.img))
+        .pipe(browserSync.stream());
 });
 
 gulp.task('fonts', function () {
     return gulp.src(config.build.src.fonts)
         .pipe(gulpNewer(config.build.dest.fonts))
-        .pipe(gulp.dest(config.build.dest.fonts));
+        .pipe(gulp.dest(config.build.dest.fonts))
+        .pipe(browserSync.stream());
 });
 
 gulp.task('build', ['img', 'fonts'], function () {
-    gulp.start(['html', 'css', 'js']);
+    gulp.start(['templates', 'css', 'js']);
 });
 
 gulp.task('clean', function () {
@@ -110,6 +122,18 @@ gulp.task('clean', function () {
     ], function () {
         console.log('Files deleted');
     });
+});
+
+gulp.task('watch', function () {
+    browserSync.init({
+        server: './web'
+    });
+
+    gulp.watch(config.watch.templates, ['templates']);
+    gulp.watch(config.watch.css.app, ['css-app']);
+    gulp.watch(config.watch.js.app, ['js-app']);
+    gulp.watch(config.watch.img, ['img']);
+    gulp.watch(config.watch.fonts, ['fonts']);
 });
 
 gulp.task('default', ['build']);
