7703714e459ef31d014bfa2a103bdabde29fa0cc
Ivan Podorozhny <ivan@podorozhny.ru>
2015-09-01 22:11:17 +0300

    sidebar scroll to active element

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,65 +3,79 @@ var App = function () {
         var $sidebar      = $('.sidebar'),
             initialBottom = $sidebar.css('bottom'),
             initialTop    = $sidebar.css('top');
-
+        
         var move = function () {
             var bottom = parseInt(initialBottom) - ($(document).height() - $(window).height() - $(window).scrollTop()),
                 top    = parseInt(initialTop) - $(window).scrollTop(),
                 left   = -$(window).scrollLeft();
-
+            
             if (bottom < 0) {
                 bottom = 0;
             }
-
+            
             if (top < 0) {
                 top = 0;
             }
-
+            
             $sidebar.css('bottom', bottom);
             $sidebar.css('top', top);
             $sidebar.css('left', left);
         };
-
+        
         $(window).scroll(move);
-
+        
         move();
     };
+    
+    var scrollSideBar = function () {
+        var $sidebar    = $('.sidebar'),
+            $activeItem = $sidebar.find('li.active').first();
 
+        if ($activeItem.length != 1) {
+            return false;
+        }
+
+        var scrollTo = $activeItem.offset().top - $(window).scrollTop() - ($sidebar.height() / 2) - ($activeItem.height() / 2);
+
+        $sidebar.animate({ scrollTop: scrollTo }, 500);
+    };
+    
     var configureMoment = function () {
         moment.locale('ru');
     };
-
+    
     var jGrowlOptions = {
         closerTemplate: '<div>[ закрыть все ]</div>'
     };
-
+    
     var jGrowlSuccess = function (message) {
         $.jGrowl(message, $.extend(jGrowlOptions, {
             theme: 'jgrowl-success'
         }));
     };
-
+    
     var jGrowlInfo = function (message) {
         $.jGrowl(message, $.extend(jGrowlOptions, {
             theme: 'jgrowl-info'
         }));
     };
-
+    
     var jGrowlWarning = function (message) {
         $.jGrowl(message, $.extend(jGrowlOptions, {
             theme: 'jgrowl-warning'
         }));
     };
-
+    
     var jGrowlDanger = function (message) {
         $.jGrowl(message, $.extend(jGrowlOptions, {
             theme: 'jgrowl-danger'
         }));
     };
-
+    
     return {
         init:    function () {
             initSideBar();
+            scrollSideBar();
             configureMoment();
         },
         success: function (message) {
